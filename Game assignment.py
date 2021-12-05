916febb1b4e845e1fa01aaf4ea7289b6f1d3711e
ElyasSequeira <90629095+ElyasSequeira@users.noreply.github.com>
2021-12-04 21:46:09 -0500

    Add files via upload

--- Game assignment.py
+++ Game assignment.py
@@ -1,14 +1,14 @@
 
-user_name = input("Please enter in your name: ")
-ready = input(("hi there, "+ user_name + "! welcome to your very own story adventure, where your actions will determine the results of the story! Ready to begin?(Yes/No) "))
+user_name = input("Hello,\nPlease enter in your name: ")
+ready = input(("Hi there, "+ str.capitalize(user_name) + "! welcome to your very own story adventure, where your actions will determine the results of the story! Ready to begin?(Yes/No) "))
 
 if ready.lower() != "yes":
     print("no problem, see you next time!")
     quit()
 
-print("lets begin!")
+print("\nlets begin!")
 
-answer = input("Today's day is September 7th, 1990. You have just finished your work day and have decided to head back home, which is 30 minutes away by walking, or 10 minutes away by cab. You think about which option you would like to choose.(cab / walk) ")
+answer = input("\nToday's day is September 7th, 1990. You have just finished your work day and have decided to head back home, which is 30 minutes away by walking, or 10 minutes away by cab. You think about which option you would like to choose.(cab / walk)\n")
 
 if answer.lower() == "walk":               #Path for WALK
     
@@ -90,13 +90,20 @@ if answer.lower() == "walk":               #Path for WALK
     else:
         print("Invalid input. Please ensure all spelling is correct.")
 
-elif answer == "cab":                                           #PATH FOR CAB KIDNAPPED
-    print("you cabbed.")
+elif answer.lower() == "cab":          #PATH FOR CAB KIDNAPPED
+ 
+    answer4 = input("\nYou wave for a cab. After a few minutes, a cab pulls over, as it apporaches, the bright yellow lights turn the car into a shillouette. The driver rolls down the window and asks you for your destination. you tell him your address and he signals you to get in.\nAfter getting into the cab's backseat, you fasten your seatbelt and he signals to enter back onto the roadway. The driver begins to make small talk.\n\n'So, where you from? What's your name?'\n\nYou're not the most trusting person, so you think about whether you should lie or tell the truth.(lie/truth)\n ")
+    
+    if answer4.lower() == "lie":
+        print("\n(You decide to lie)\n\nYou tell the man that your name is Alex and that you're from a few cities over, and are heading to your brother's house for a dinner. You talk back and forth until you notice that the taxi driver misses the turn onto your street. You correct his mistake and he replies in an unconvincing tone.\n'Oh dang! I'm sorry! I know another route dont worry.")
+        
+        lie1 = input("\nYou start to watch the driver carefully, and he continues to stray further from your address. You grow concerned and begin debating whether or not you should ask the driver to pull over.\nAfter another minute, you notice the driver is making no attempt to go to your destination, your concern grows to a deep and anxious worry.\nYou stop thinking and decide to tell the man to:(pull over/turn around)\n ")
+    
+    elif answer4.lower() == "truth":
+        print("You decided to tell the truth.")
 
 else:
     print("not an option try  big again")
-    print ("test change")
 
 
 
-#TEST TEST COMMIT 
